835b61a2ea36da67f9dade0e77fc423c13710f7f
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-03-15 17:07:15 -0400

    commit tree 95bf24ec82a1136bece830733e86d1eacb823539

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,20 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+  before_save :email_author_about_post
+
   private
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
+  end
+
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
   end
 
   def make_title_case
